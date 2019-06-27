501642209e5aa4ee90b6823e8c8f9011c8af53ed
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-26 22:59:10 -0700

    updated to take heroku variable for db connection

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(bodyParser.json());
 
 // DB config
-const db = require('./config/keys').mongoURI;
+// const db = require('./config/keys').mongoURI;
+const db = process.env.SECRET || require('./config/keys').mongoURI;
 
 // Connect to mongo
 mongoose
@@ -25,7 +26,7 @@ app.use('/api/highscores', highscores);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
