d2a88e084e3951010ea8260b306ba51430da07b3
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-27 19:58:13 -0700

    removed unecessary redux connect and converted to class component

--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -1,30 +1,38 @@
+// React
 import React from 'react';
-import { connect } from 'react-redux';
+
+// CSS
 import styles from '../css-modules/Square.module.css'
 
-function Square(props) {
-  let valueToDisplay;
-  const thisSquare = props.state;
-  if (!thisSquare) return <button></button>;
-
-  if (thisSquare && thisSquare.isRevealed) {
-    valueToDisplay = thisSquare.value ? thisSquare.value : null;
-  }
-  else if (thisSquare && thisSquare.isFlagged) {
-    valueToDisplay = <img className={styles.flag} src="/favicon.ico" alt="flag"/>
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  const squareStyle = props.index % 2 ? styles.oddSquare : styles.evenSquare;
+  render() {
+    let valueToDisplay;
+    const thisSquare = this.props.state;
+    if (!thisSquare) return <button></button>;
 
-  return (
-    <button
-      className={!thisSquare.isRevealed ? squareStyle : styles.revealedSquare}
-      onClick={() => props.handleClick(props.index)}
-      onContextMenu={e => { e.preventDefault(); props.handleFlag(props.index);}}
-    >
-      {valueToDisplay === 'b' ? <i className="fa fa-bomb"/> : valueToDisplay}
-    </button>
-  );
+    if (thisSquare && thisSquare.isRevealed) {
+      valueToDisplay = thisSquare.value ? thisSquare.value : null;
+    }
+    else if (thisSquare && thisSquare.isFlagged) {
+      valueToDisplay = <img className={styles.flag} src="/favicon.ico" alt="flag"/>
+    }
+
+    const squareStyle = this.props.index % 2 ? styles.oddSquare : styles.evenSquare;
+
+    return (
+      <button
+        className={!thisSquare.isRevealed ? squareStyle : styles.revealedSquare}
+        onClick={() => this.props.handleClick(this.props.index)}
+        onContextMenu={e => { e.preventDefault(); this.props.handleFlag(this.props.index);}}
+      >
+        {valueToDisplay === 'b' ? <i className="fa fa-bomb"/> : valueToDisplay}
+      </button>
+    );
+  }
 }
 
-export default connect()(Square);
+export default Square;
